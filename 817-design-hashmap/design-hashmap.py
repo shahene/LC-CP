ef8c29a9b994e42152e3b50a87be5fa54ad2977e
shahene <120350676+shahene@users.noreply.github.com>
2025-11-18 01:03:24 -0800

    Time: 610 ms (11.43%) | Memory: 44 MB (5.64%) - LeetSync

--- 817-design-hashmap/design-hashmap.py
+++ 817-design-hashmap/design-hashmap.py
@@ -1,37 +1,19 @@
-class ListNode:
-    def __init__(self, key=-1, val=-1, next=None):
-        self.key = key
-        self.val = val
-        self.next = None
 class MyHashMap:
-    def hash(self, key):
-        return key % 10000
+
     def __init__(self):
-        self.my_map = [ListNode() for _ in range(10000)]
+        self.arr = [-1 for _ in range(1000000)]
 
     def put(self, key: int, value: int) -> None:
-        cur = self.my_map[self.hash(key)]
-        while cur.next:
-            if cur.next.key == key:
-                cur.next.val = value
-                return
-            cur = cur.next
-        cur.next = ListNode(key=key, val=value)
+        self.arr[key - 1] = value
 
     def get(self, key: int) -> int:
-        cur = self.my_map[self.hash(key)]
-        while cur.next:
-            if cur.next.key == key:
-                return cur.next.val
-            cur = cur.next
-        return -1
+        return self.arr[key - 1]
+        
 
     def remove(self, key: int) -> None:
-        cur = self.my_map[self.hash(key)]
-        while cur and cur.next:
-            if cur.next.key == key:
-                cur.next = cur.next.next
-            cur = cur.next
+        self.arr[key - 1] = -1
+        
+
 
 # Your MyHashMap object will be instantiated and called as such:
 # obj = MyHashMap()
