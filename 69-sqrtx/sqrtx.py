7d4ba6d36e30c6bc6cc61f52fba7be3c0ecb4cc3
shahene <120350676+shahene@users.noreply.github.com>
2025-11-19 23:32:40 -0800

    Time: 4 ms (42.01%) | Memory: 17.8 MB (50.42%) - LeetSync

---
+++ 69-sqrtx/sqrtx.py
@@ -0,0 +1,19 @@
+import math
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x < 2: return x
+
+        l, r = 0, x
+        while l <= r:
+            mid = (l + r) // 2
+            mid_calculated = mid * mid
+            if mid_calculated == x:
+                return math.floor(mid)
+            elif mid_calculated > x:
+                r = mid - 1
+            else:
+                l = mid + 1
+        # r is the largest intger since (r < l after loop ends) such that r * r <= x
+        return math.floor(r)
+            
+        
