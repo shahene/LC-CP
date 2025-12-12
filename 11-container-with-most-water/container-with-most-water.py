d52d765128cd2cf88b07a3bc6f02126bd9cb899a
shahene <120350676+shahene@users.noreply.github.com>
2025-12-11 20:01:39 -0800

    Time: 99 ms (68.27%) | Memory: 28.4 MB (90.32%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,12 +1,20 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
+        '''
+        start wide
+        calculate area
+        strategically move l/r up/down depending on highest bar
+        return max area
+        O(n) time + O(1) space
+        '''
         l, r = 0, len(height) - 1
-        max_area = -math.inf
+        max_area = 0
         while l < r:
             cur_area = min(height[l], height[r]) * (r - l)
-            max_area = max(cur_area, max_area)
+            max_area = max(max_area, cur_area)
             if height[l] > height[r]:
                 r -= 1
             else:
                 l += 1
+                
         return max_area
