bcf2655ff72e68d57084b8579b32f020dd1d628e
shahene <120350676+shahene@users.noreply.github.com>
2026-01-11 16:16:59 -0800

    Time: 633 ms (33.96%) | Memory: 22.4 MB (5.05%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,22 +1,32 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        ans = []
+        '''
+        outer loop to iterate through array, fix nums[i]
+        if i != 0 and nums[i] == nums[i-1]: continue
+        nums[i] + nums[j] + nums[k] == 0
+        looking for above
+        if < 0, increment l
+        if > 0, decrement r
+        if total == 0:
+            increment l while nums[l] == nums[l-1] and l < len(nums)
+        
+        O(n^2) time + O(1) space
+        '''
+        output = []
         nums.sort()
         for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i-1]:
-                continue
-            l, r = i + 1, len(nums) - 1
-            target = -1 * nums[i]
-            while l < r:
-                total = nums[l] + nums[r]
-                if total > target:
-                    r -= 1
-                elif total < target:
-                    l += 1
+            if i > 0 and nums[i] == nums[i-1]: continue
+            j, k = i + 1, len(nums) - 1
+            while j < k:
+                total = nums[i] + nums[j] + nums[k]
+                if total < 0:
+                    j += 1
+                elif total > 0:
+                    k -= 1
                 else:
-                    ans.append([nums[i], nums[l], nums[r]])
-                    l += 1
-                    r -= 1
-                    while nums[l] == nums[l-1] and l < r:
-                        l += 1
-        return ans
+                    output.append([nums[i], nums[j], nums[k]])
+                    j += 1
+                    k -= 1
+                    while j < len(nums) and nums[j] == nums[j-1]:
+                        j += 1
+        return output
