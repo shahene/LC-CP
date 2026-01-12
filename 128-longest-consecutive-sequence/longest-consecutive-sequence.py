916fe3e05f38dca857539417b00f351dea426447
shahene <120350676+shahene@users.noreply.github.com>
2026-01-11 17:02:20 -0800

    Time: 43 ms (85.76%) | Memory: 36.7 MB (15.92%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -8,15 +8,12 @@ class Solution:
         3-> 2
         2-> 1
         '''
-        num_map = {}
+        num_set = set(nums)
         max_length = 0
-        for n in nums:
-            num_map[n] = n + 1
-        for n in num_map:
-            if num_map[n] not in num_map:
+        for n in num_set:
+            if n - 1 not in num_set:
                 length = 0
-                while n - length in num_map:
+                while n + length in num_set:
                     length += 1
-                max_length = max(max_length, length)
+                max_length = max(max_length , length)
         return max_length
-        
