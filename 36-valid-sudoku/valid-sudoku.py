2c49baf24cb010c7582ff0104791a0dcfe50d9e2
shahene <120350676+shahene@users.noreply.github.com>
2026-01-17 00:38:54 -0800

    Time: 11 ms (8.46%) | Memory: 19.5 MB (8.27%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,18 +1,18 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        cols = collections.defaultdict(set)
-        rows = collections.defaultdict(set)
-        squares = collections.defaultdict(set)
-
-        for r in range(len(board)):
-            for c in range(len(board[0])):
-                if board[r][c] == '.': continue
-                if board[r][c] in rows[r] or board[r][c] in cols[c] \
-                or board[r][c] in squares[(r // 3, c // 3)]:
+        '''
+        use set for columns, set for rows, and set for 3*3 boxes
+        3*3 boxes -> key for set will be (r // 3, c // 3) as tuple 
+        O(81) -> O(81) time O(9) space -> O(1) time and O(1) space
+        '''
+        row_set, col_set, box_set = collections.defaultdict(set), collections.defaultdict(set), collections.defaultdict(set)
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] in row_set[r] or board[r][c] in col_set[c] or board[r][c] in box_set[(r // 3, c // 3)]:
                     return False
-                
-                rows[r].add(board[r][c])
-                cols[c].add(board[r][c])
-                squares[(r // 3, c // 3)].add(board[r][c])
+                if board[r][c] != '.':
+                    row_set[r].add(board[r][c])
+                    col_set[c].add(board[r][c])
+                    box_set[(r // 3, c // 3)].add(board[r][c])
         return True
 
