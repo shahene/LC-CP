2bfb62843fcc53edb3cdec5a5c476c06c5987f01
shahene <120350676+shahene@users.noreply.github.com>
2026-01-22 23:55:48 -0800

    Time: 63 ms (73.71%) | Memory: 29.6 MB (19.26%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,20 +1,26 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         '''
-        start wide
-        calculate area
-        strategically move l/r up/down depending on highest bar
-        return max area
-        O(n) time + O(1) space
+        given an integer array height of length n. There are n vertical lines 
+        drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i])
+        find two lines that together with the x-axis form a container, such that the 
+        container contains the most water
+        return maximum amount of water
+
+        greedy:
+        two pointers, start from both ends
+        min_height from either endpoint is the constraint
+        container would be (r - l) * min_height
+        increment the pointer that points to the lower height [greedy]
+        
+        time: O(n), space O(1)
         '''
         l, r = 0, len(height) - 1
-        max_area = 0
+        max_container = 0
         while l < r:
-            cur_area = min(height[l], height[r]) * (r - l)
-            max_area = max(max_area, cur_area)
-            if height[l] > height[r]:
-                r -= 1
-            else:
+            max_container = max(max_container, min(height[l], height[r]) * (r - l))
+            if height[l] < height[r]:
                 l += 1
-                
-        return max_area
+            else:
+                r -= 1
+        return max_container
