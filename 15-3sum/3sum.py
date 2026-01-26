6d87cc3190767e21f94abe5f09023c4c0c0141b5
shahene <120350676+shahene@users.noreply.github.com>
2026-01-25 18:04:58 -0800

    Time: 631 ms (40.88%) | Memory: 22.3 MB (29.64%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,32 +1,30 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         '''
-        outer loop to iterate through array, fix nums[i]
-        if i != 0 and nums[i] == nums[i-1]: continue
+        given an integery arary nums, return all triplets 
+        solution should not contain duplicate triplets
         nums[i] + nums[j] + nums[k] == 0
-        looking for above
-        if < 0, increment l
-        if > 0, decrement r
-        if total == 0:
-            increment l while nums[l] == nums[l-1] and l < len(nums)
-        
-        O(n^2) time + O(1) space
+        sort nums
+        use two pointers to find other two numbers
+        skip duplicates (which would be same number at same position)
         '''
-        output = []
         nums.sort()
+        solution_set = []
         for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i-1]: continue
-            j, k = i + 1, len(nums) - 1
-            while j < k:
-                total = nums[i] + nums[j] + nums[k]
+            if i != 0 and nums[i] == nums[i-1]: continue
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                total = nums[l] + nums[r] + nums[i]
                 if total < 0:
-                    j += 1
+                    l += 1
                 elif total > 0:
-                    k -= 1
+                    r -= 1
                 else:
-                    output.append([nums[i], nums[j], nums[k]])
-                    j += 1
-                    k -= 1
-                    while j < len(nums) and nums[j] == nums[j-1]:
-                        j += 1
-        return output
+                    solution_set.append([nums[l], nums[r], nums[i]])
+                    l += 1
+                    r -= 1
+                    while l < len(nums) and nums[l] == nums[l-1]:
+                        l += 1
+            
+
+        return solution_set
