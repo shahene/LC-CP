0d8315f84d5d239fa5cfd673e387e34e02dbd2f6
shahene <120350676+shahene@users.noreply.github.com>
2026-01-28 21:58:51 -0800

    Time: 70 ms (99.32%) | Memory: 76.9 MB (88.03%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,20 +1,33 @@
+'''
+basically, i'd want to keep track of which is the oldest unused element
+when it is used, mark it as most recently used
+if len(map) > capacity: pop lease recently used
+
+OrderedDiction -> from collections import OrderedDict
+Reordering keys dynamically with move_to_end() (useful for FIFO/LIFO access).
+Popping items from either end with popitem(last=True/False).
+
+'''
+
 from collections import OrderedDict
 class LRUCache:
-
-    def __init__(self, capacity: int):
+    def __init__(self, capacity):
         self.capacity = capacity
-        self.ordered_dict = OrderedDict()
+        self.ordered_cache = OrderedDict()
     def get(self, key: int) -> int:
-        if key in self.ordered_dict:
-            self.ordered_dict.move_to_end(key, last=True)
-            return self.ordered_dict[key]
-        return -1
-    def put(self, key: int, value: int) -> None:
-        self.ordered_dict[key] = value
-        self.ordered_dict.move_to_end(key, last=True)
-        while len(self.ordered_dict) > self.capacity:
-            self.ordered_dict.popitem(last=False)
+        if key in self.ordered_cache:
+            self.ordered_cache.move_to_end(key)
+            return self.ordered_cache[key]
+        return -1 
+    def put(self, key:int, value:int) -> None:
         
+        self.ordered_cache[key] = value
+        self.ordered_cache.move_to_end(key)
+        if len(self.ordered_cache) > self.capacity:
+            self.ordered_cache.popitem(last=False)
+        
+    
+
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
