f8e60ac01d48e6024bdf4422bdb381b105ce8a94
shahene <120350676+shahene@users.noreply.github.com>
2026-01-29 23:36:49 -0800

    Time: 125 ms (62.95%) | Memory: 78.9 MB (7.13%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,34 +1,49 @@
-'''
-basically, i'd want to keep track of which is the oldest unused element
-when it is used, mark it as most recently used
-if len(map) > capacity: pop lease recently used
+# Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
 
-OrderedDiction -> from collections import OrderedDict
-Reordering keys dynamically with move_to_end() (useful for FIFO/LIFO access).
-Popping items from either end with popitem(last=True/False).
+# Implement the LRUCache class:
 
-'''
+# LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
+# int get(int key) Return the value of the key if the key exists, otherwise return -1.
+# void put(int key, int value) Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache. If the number of keys exceeds the capacity from this operation, evict the least recently used key.
+# The functions get and put must each run in O(1) average time complexity.
+class Node:
+    def __init__(self, key, val, prev=None, next=None):
+        self.key = key
+        self.val = val
+        self.prev = prev
+        self.next = next
 
-from collections import OrderedDict
 class LRUCache:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.ordered_cache = OrderedDict()
-    def get(self, key: int) -> int:
-        if key in self.ordered_cache:
-            self.ordered_cache.move_to_end(key)
-            return self.ordered_cache[key]
-        return -1 
-    def put(self, key:int, value:int) -> None:
-        
-        self.ordered_cache[key] = value
-        self.ordered_cache.move_to_end(key)
-        if len(self.ordered_cache) > self.capacity:
-            self.ordered_cache.popitem(last=False)
-        
-    
+        self.cache = {}
+        self.left, self.right = Node(0, 0), Node(0, 0)
+        self.left.next, self.right.prev = self.right, self.left
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next = nxt
+        nxt.prev = prev
+    def insert(self, node):
+        # need to insert at right
+        prev, nxt = self.right.prev, self.right
+        prev.next = node
+        node.prev = prev
+        node.next = nxt
+        nxt.prev = node
+    def get(self, key):
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
+        return -1
+    def put(self, key, value):
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key,value)
+        self.insert(self.cache[key])
 
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+        if len(self.cache) > self.capacity:
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
+            
