7085b5661df68b5c18738488fb5f2d11a04b549c
shahene <120350676+shahene@users.noreply.github.com>
2026-01-30 17:56:19 -0800

    Time: 288 ms (57.14%) | Memory: 19.4 MB (40.95%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,34 +1,32 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         '''
-        brute force --> O(n^3)
-        O(n) palindromic checking * O(n^2) checking every single substring in s
-
-        key: palindromic checking from middle + expanding outward
-        caveat: even length palindromes, set l = i & r = i + 1
-                effectively checks two characters at a time and not 1 in beginning
-        optimized --> O(n^2)
-        O(n) processing * O(n) palindromic checking
-
-        Auxiliary Space (not counting input or output) --> O(1)
-        '''
-        res_length = 0
+        process string
+        start from character, expand leftwards and rightwards
+        if mismatch;  
+        resLength = 0
         res = ''
+        do for both odd and even length strings
+        while loop checking if palindrome so far (out of bounds etc)
+        if r - l + 1 > resLength:
+            resLength = r - l + 1
+            res = s[l: r + 1]
+        '''
+        res_length, res = 0, ''
         for i in range(len(s)):
             l, r = i, i
-            while l >= 0 and r < len(s) and s[r] == s[l]:
-                # odd length
+            while l >= 0 and r < len(s) and s[l] == s[r]:
                 if (r - l + 1) > res_length:
                     res_length = r - l + 1
                     res = s[l: r + 1]
                 l -= 1
                 r += 1
             l, r = i, i + 1
-            while l >= 0 and r < len(s) and s[r] == s[l]:
-                # even length
+            while l >= 0 and r < len(s) and s[l] == s[r]:
                 if (r - l + 1) > res_length:
                     res_length = r - l + 1
                     res = s[l: r + 1]
-                l -= 1
+                l -= 1 
                 r += 1
         return res
+
