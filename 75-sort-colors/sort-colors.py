7369defa9d38b94b1dc56bc90716f605cb67e156
shahene <120350676+shahene@users.noreply.github.com>
2026-02-02 15:30:00 -0800

    Time: 0 ms (100.00%) | Memory: 19.3 MB (32.33%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,17 +3,32 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        '''
+        n objects colored red, white or blue
+        sort them in place
+        so that objects of the same color are adjacent
+
+        0, 1, 2 -> in that order
+
+        ok so we can have two pointers
+        l, r 
+        whenever we see a 0, we insert 0 into nums[l], increment l (increment i)
+        whenever we see a 2, we insert 2 into nums[r], decrement r but don't increment i
+        whenever we see 1, just skip (increment i) 
+
+        our main condition will be while i < r
+        this will be in-place so O(n) time + O(1) space
+        '''
         l, r = 0, len(nums) - 1
         index = 0
         while index <= r:
-            if nums[index] == 0:
-                nums[l], nums[index] = nums[index], nums[l]
+            val = nums[index]
+            if val == 0:
+                nums[index], nums[l] = nums[l], nums[index]
                 l += 1
                 index += 1
-            elif nums[index] == 1:
+            elif val == 1:
                 index += 1
-                continue
             else:
                 nums[index], nums[r] = nums[r], nums[index]
                 r -= 1
-
