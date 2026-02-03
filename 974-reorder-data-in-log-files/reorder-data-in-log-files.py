71b142e2697e679d04dafebb96a1e4f6cfaac1ea
shahene <120350676+shahene@users.noreply.github.com>
2026-02-02 17:56:26 -0800

    Time: 0 ms (100.00%) | Memory: 19.6 MB (17.82%) - LeetSync

--- 974-reorder-data-in-log-files/reorder-data-in-log-files.py
+++ 974-reorder-data-in-log-files/reorder-data-in-log-files.py
@@ -2,12 +2,10 @@ class Solution:
     def reorderLogFiles(self, logs: List[str]) -> List[str]:
         letter_logs, digit_logs = [], []
         for log in logs:
-            # check if letter or digit
             first_content = log.split()[1]
             if first_content.isdigit():
                 digit_logs.append(log)
             else:
                 letter_logs.append(log)
-
         letter_logs.sort(key=lambda x: (x.split(' ', 1)[1], (x.split()[0])))
-        return letter_logs + digit_logs 
+        return letter_logs + digit_logs
