bc243cd92c6e29ec75b895ae3682fe55de5600b3
shahene <120350676+shahene@users.noreply.github.com>
2026-02-02 16:53:07 -0800

    Time: 144 ms (38.37%) | Memory: 78.8 MB (13.09%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,49 +1,58 @@
-# Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
-
-# Implement the LRUCache class:
-
-# LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
-# int get(int key) Return the value of the key if the key exists, otherwise return -1.
-# void put(int key, int value) Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache. If the number of keys exceeds the capacity from this operation, evict the least recently used key.
-# The functions get and put must each run in O(1) average time complexity.
 class Node:
-    def __init__(self, key, val, prev=None, next=None):
+    def __init__(self, key, val):
         self.key = key
         self.val = val
-        self.prev = prev
-        self.next = next
-
+        self.prev, self.next = None, None
 class LRUCache:
-    def __init__(self, capacity):
+
+    def __init__(self, capacity: int):
         self.capacity = capacity
-        self.cache = {}
-        self.left, self.right = Node(0, 0), Node(0, 0)
-        self.left.next, self.right.prev = self.right, self.left
+        self.cache = {} # map of key to nodes
+        self.left, self.right = Node(-1, -1), Node(-1, -1)
+        # initially want left and right connected so we can insert
+        # between them
+        self.left.next = self.right
+        self.right.prev = self.left
+
     def remove(self, node):
+        '''
+        imagine doubly linked list
+        to remove node
+        just assign its prev to next
+        assign next to its prev
+        [] => [] => []
+        '''
         prev, nxt = node.prev, node.next
         prev.next = nxt
         nxt.prev = prev
-    def insert(self, node):
-        # need to insert at right
+    def insert(self, node): # want to insert at the right
         prev, nxt = self.right.prev, self.right
         prev.next = node
-        node.prev = prev
-        node.next = nxt
         nxt.prev = node
-    def get(self, key):
+        node.next = nxt
+        node.prev = prev
+
+    def get(self, key: int) -> int:
         if key in self.cache:
             self.remove(self.cache[key])
             self.insert(self.cache[key])
             return self.cache[key].val
         return -1
-    def put(self, key, value):
+    def put(self, key: int, value: int) -> None:
         if key in self.cache:
             self.remove(self.cache[key])
-        self.cache[key] = Node(key,value)
+        self.cache[key] = Node(key, value)
         self.insert(self.cache[key])
-
         if len(self.cache) > self.capacity:
+            # remove from list, delete from map => remove lru, delete lru
             lru = self.left.next
             self.remove(lru)
             del self.cache[lru.key]
-            
+        
+        
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
