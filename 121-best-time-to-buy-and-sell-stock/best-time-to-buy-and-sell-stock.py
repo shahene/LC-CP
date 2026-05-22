8d50e5de4e4ec06b92c42ead55fe335267de671a
shahene <120350676+shahene@users.noreply.github.com>
2026-05-21 19:15:05 -0700

    Time: 48 ms (57.39%) | Memory: 28.7 MB (22.26%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,18 +1,12 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        '''
-        choose a single day to buy one stock
-        choose a different day in the future to sell that stock
-        return maximum profit you can achieve from this transaction.
-        [7,1,5,3,6,4]
-
-        '''
-        if len(prices) == 1: return 0
-        l, max_profit = 0, 0
+        # sliding window
+        l = 0
+        max_profit = 0
         for r in range(1, len(prices)):
-            if prices[r] < prices[l]:
+            profit = prices[r] - prices[l]
+            if profit <= 0:
                 l = r
             else:
-                max_profit = max(max_profit, prices[r] - prices[l])
+                max_profit = max(max_profit, profit)
         return max_profit
-        
