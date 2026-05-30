df4ab5ef45a5131bdeb5747145e06c298f678814
shahene <120350676+shahene@users.noreply.github.com>
2026-05-29 20:36:17 -0700

    Time: 20 ms (19.57%) | Memory: 19.2 MB (68.58%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,13 +1,25 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        '''
+        sliding window
+        keep hashmap -- count of characters
+        if count exceeds 1 for any character, duplicate found
+        then we must delete character from the left, decrementing count as we go
+        until that character's count is 1 or less
+        keep track of length as you go
+        O(n) time + O(n) space 
+        '''
+        count_hashmap = {}
+        max_length = 0
         l = 0
-        max_repeat_letter, count_letters = 0, {}
         for r in range(len(s)):
-            if s[r] not in count_letters:
-                count_letters[s[r]] = 0
-            count_letters[s[r]] += 1
-            while count_letters[s[r]] > 1:
-                count_letters[s[l]] -= 1
+            current_char = s[r]
+            if current_char not in count_hashmap:
+                count_hashmap[current_char] = 0
+            count_hashmap[current_char] += 1
+            while count_hashmap[current_char] > 1:
+                left_char = s[l]
+                count_hashmap[left_char] -= 1
                 l += 1
-            max_repeat_letter = max(max_repeat_letter, r - l + 1)
-        return max_repeat_letter
+            max_length = max(max_length, r - l + 1)
+        return max_length
