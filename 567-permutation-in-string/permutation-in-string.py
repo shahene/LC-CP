e80ef6014bbdbb6b946d30f5330dfd2790084b8a
shahene <120350676+shahene@users.noreply.github.com>
2026-05-30 22:23:31 -0700

    Time: 23 ms (48.34%) | Memory: 19.7 MB (19.20%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,18 +1,18 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        orig_map = collections.Counter(s1)
+        s1_map = collections.Counter(s1)
+        s1_length = len(s1)
         l = 0
-        n_map = {}
+        counter = 0
+        s2_map = {}
         for r in range(len(s2)):
-            if s2[r] not in n_map:
-                n_map[s2[r]] = 0
-            n_map[s2[r]] += 1
-            print(n_map)
-            if r >= len(s1) - 1:
-                if n_map == orig_map:
-                    return True
-                n_map[s2[l]] -= 1
-                if n_map[s2[l]] == 0:
-                    del n_map[s2[l]]
+            if s2[r] not in s2_map:
+                s2_map[s2[r]] = 0
+            s2_map[s2[r]] += 1
+            if r - l + 1 == s1_length:
+                if s2_map == s1_map: return True
+                s2_map[s2[l]] -= 1
+                if s2_map[s2[l]] == 0: del s2_map[s2[l]]
                 l += 1
+        print(s2_map, s1_map)
         return False
