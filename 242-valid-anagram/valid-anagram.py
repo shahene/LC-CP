758ad9bfaffb85a2284d5b98f4378e854e0f95a7
shahene <120350676+shahene@users.noreply.github.com>
2026-06-01 20:09:45 -0700

    Time: 15 ms (43.09%) | Memory: 19.9 MB (20.47%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,11 +1,3 @@
-from collections import defaultdict
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t): return False
-        counter_s, counter_t = defaultdict(int), defaultdict(int)
-        for char1, char2 in zip(s, t):
-            counter_s[char1] += 1
-            counter_t[char2] += 1
-        return counter_s == counter_t
-
-        
+        return ''.join(sorted(s)) == ''.join(sorted(t))
