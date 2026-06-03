9db23960489bc282aec01ee900bce6c8251f3489
shahene <120350676+shahene@users.noreply.github.com>
2026-06-02 21:22:23 -0700

    Time: 3 ms (76.52%) | Memory: 19.4 MB (34.86%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,18 +1,18 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        '''
-        use set for columns, set for rows, and set for 3*3 boxes
-        3*3 boxes -> key for set will be (r // 3, c // 3) as tuple 
-        O(81) -> O(81) time O(9) space -> O(1) time and O(1) space
-        '''
-        row_set, col_set, box_set = collections.defaultdict(set), collections.defaultdict(set), collections.defaultdict(set)
-        for r in range(9):
-            for c in range(9):
-                if board[r][c] in row_set[r] or board[r][c] in col_set[c] or board[r][c] in box_set[(r // 3, c // 3)]:
+        # row check, col check, sub-box check
+        valid_check_rows = collections.defaultdict(set)
+        valid_check_cols = collections.defaultdict(set)
+        valid_check_subbox = collections.defaultdict(set)
+        
+        dim = len(board)
+        for r in range(len(board)):
+            for c in range(len(board)):        
+                val = board[r][c]
+                if val == '.': continue
+                if val in valid_check_rows[r] or val in valid_check_cols[c] or val in valid_check_subbox[(r // 3, c // 3)]:
                     return False
-                if board[r][c] != '.':
-                    row_set[r].add(board[r][c])
-                    col_set[c].add(board[r][c])
-                    box_set[(r // 3, c // 3)].add(board[r][c])
+                valid_check_rows[r].add(board[r][c])
+                valid_check_cols[c].add(board[r][c])
+                valid_check_subbox[(r // 3, c // 3)].add(board[r][c])
         return True
-
