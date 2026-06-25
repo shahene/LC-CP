cf228cd717554ea1d557e31f9554c8dbc1ac3ac8
Shahene Chaouachi <120350676+shahene@users.noreply.github.com>
2026-06-24 21:19:31 -0700

    Time: 79 ms (75.66%) | Memory: 19.6 MB (49.81%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,27 +1,32 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        max_length = 0
-        # choose any character + k replacements -> 
         '''
-        sliding window of some kind
-        hashmap -- keeps track of number characters in window -- use it to get max_count
-        (r - l + 1) - max_count > k ==> invalid
-        if invalid:
-            from left start sliding -- l += 1
-        if valid:
-            record max length
+        validity condition: if (current_window - max_repeating_letter) > k
+        
+        while window is invalid:
+            slide window from left (shrinks window size which would eventually make window valid
+            if that is possible)
+                decrement frequency 
+
+            what happens to max repeating letter?
+                well assuming we already recorded window size earlier (we don't need to update it)
+                because if we don't find a larger max repeating letter count in the future, we will not have a larger substring (so doesn't matter)
+        
+        O(N) time + O(N) space -- worst case k == length of s (all different characters)
+        
+        edge cases: lower case letters, empty string (must return 0 by default), k > len(s)
         '''
-        map_characters = {}
-        l = 0
-        max_char = 0
-        for r in range(len(s)):
-            if s[r] not in map_characters:
-                map_characters[s[r]] = 0
-            map_characters[s[r]] += 1 
-            max_char = max(max_char, map_characters[s[r]])
-            while ((r - l + 1) - max_char) > k:
-                map_characters[s[l]] -= 1
+        freq_map = collections.defaultdict(int)
+        l, max_length = 0, 0
+        max_repeating_letter = 0
+        for r, n in enumerate(s):
+            freq_map[n] += 1
+            max_repeating_letter = max(max_repeating_letter, freq_map[n])
+            while (r - l + 1) - max_repeating_letter > k:
+                left_char = s[l]
+                freq_map[left_char] -= 1
                 l += 1
-            
             max_length = max(max_length, r - l + 1)
         return max_length
+
+
