7ab9f2a22036bf093fd1c56d0fb53d69a1081c79
Shahene Chaouachi <120350676+shahene@users.noreply.github.com>
2026-06-25 21:44:34 -0700

    Time: 15 ms (81.28%) | Memory: 19.3 MB (85.92%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,18 +1,49 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        s1_map = collections.Counter(s1)
-        s1_length = len(s1)
+        '''
+        s1 = "ab"
+        s2 = "eidbaooo"
+
+        s1_frequency_map = {
+            "a": 1,
+            "b": 1
+        }
+
+
+        
+        if len(s1) > len(s2): return False
+        
+        s2_frequency_map = {}
+        sliding window
         l = 0
-        counter = 0
-        s2_map = {}
-        for r in range(len(s2)):
-            if s2[r] not in s2_map:
-                s2_map[s2[r]] = 0
-            s2_map[s2[r]] += 1
-            if r - l + 1 == s1_length:
-                if s2_map == s1_map: return True
-                s2_map[s2[l]] -= 1
-                if s2_map[s2[l]] == 0: del s2_map[s2[l]]
+        iterate through s2 with r 
+            add each character to s2 frequency w relevant frequency
+        once we reach length k (k = len(s1)):
+            if s2_frequency_map == s1_frequency_map: return True
+                look at character at l 
+                decrement frequency
+                if s2_frequency_map[char at l] == 0 del from map
                 l += 1
-        print(s2_map, s1_map)
+        
+        return False
+
+        '''
+        if len(s1) > len(s2): return False
+        l = 0
+
+        s1_frequency_map = collections.Counter(s1)
+        s2_frequency_map = collections.defaultdict(int)
+
+        for r in range(len(s2)):
+            current_char = s2[r]
+            s2_frequency_map[current_char] += 1
+
+            window = r - l + 1
+            if window >= len(s1):
+                if s2_frequency_map == s1_frequency_map: return True
+                left_character = s2[l]
+                s2_frequency_map[left_character] -= 1
+                if s2_frequency_map[left_character] == 0: del s2_frequency_map[left_character]
+                l += 1
+                
         return False
