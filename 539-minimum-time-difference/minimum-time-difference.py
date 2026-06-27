3c048e1dc9eeb13fea1b31e7c144f88088181eb0
Shahene Chaouachi <120350676+shahene@users.noreply.github.com>
2026-06-26 17:10:49 -0700

    Time: 8 ms (56.97%) | Memory: 21.1 MB (87.18%) - LeetSync

--- 539-minimum-time-difference/minimum-time-difference.py
+++ 539-minimum-time-difference/minimum-time-difference.py
@@ -1,20 +1,40 @@
 class Solution:
     def findMinDifference(self, timePoints: List[str]) -> int:
         '''
-        convert all times to minutes since midnight
-        sort -> find min minute difference
+        given list of 24 hour clock time points in "HH:MM"
+        return minimum minutes difference between any two time-points in the list
+        
+        we need to standardize HH:MM to minutes since midnight
+        need first two digits * 60 = minutes
+        last two digits also equals minutes
+
+        sort 
+        find min difference from adjacent times
+        think about wrap around time which would be:
+        how many minutes at midnight index of midnight is 24 hours * 60
+        1440 - last time + first time
         '''
-        # conversion to minutes since midnight
-        normalized_time = []
-        for time in timePoints:
-            split_time = time.split(':')
-            hour_minute, minute = int(split_time[0]) * 60, int(split_time[1])
-            normalized_time.append(hour_minute + minute)
-        normalized_time.sort()
-        min_time = math.inf
-        for i in range(1, len(normalized_time)):
-            current_diff = abs(normalized_time[i] - normalized_time[i-1])
-            min_time = min(min_time, current_diff)
-            
-        min_time = min(min_time, 1440 - normalized_time[-1] + normalized_time[0])
-        return min_time
+        def military_minutes(time):
+            time_ = time.split(':')
+
+            hours, minutes = int(time_[0]), int(time_[1])
+            total_minutes = (hours * 60) + minutes
+
+            return total_minutes
+        
+        for i, time in enumerate(timePoints):
+            total_minutes = military_minutes(time)
+            timePoints[i] = total_minutes
+        
+        timePoints.sort()
+
+        minimum_difference = math.inf
+
+        for i in range(1, len(timePoints)):
+            minimum_difference = min(minimum_difference, timePoints[i] - timePoints[i-1])
+
+        minimum_difference = min(minimum_difference, 1440 - timePoints[-1] + timePoints[0])
+
+        return minimum_difference
+       
+
