6dcbf9fdd25d8c3d065816e7ec2ec4d17a4d36c9
Shahene Chaouachi <120350676+shahene@users.noreply.github.com>
2026-06-26 18:59:30 -0700

    Time: 75 ms (48.89%) | Memory: 19.9 MB (11.87%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,26 +1,23 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        substr_start, minLength = 0, math.inf
-        t_counter = collections.Counter(t)
-        matchings = 0
-        l = 0
+        matched, substr_start, t_frequency_map, substr_max, l = 0, 0, collections.Counter(t), math.inf, 0
         for r in range(len(s)):
-            cur_char = s[r]
-            if cur_char in t_counter:
-                t_counter[cur_char] -= 1
-                if t_counter[cur_char] >= 0:
-                    matchings += 1
-                
-            while matchings == len(t):
-                if minLength > r - l + 1:
-                    minLength = r - l + 1
+            current_char = s[r]
+            if current_char in t_frequency_map:
+                t_frequency_map[current_char] -= 1
+                if t_frequency_map[current_char] >= 0:
+                    matched += 1
+            while matched == len(t) and l <= r:
+                if r - l + 1 < substr_max:
+                    substr_max = r - l + 1
                     substr_start = l
-                left_char = s[l]
-                l += 1
-                if left_char in t_counter:
-                    if t_counter[left_char] == 0:
-                        matchings -= 1
-                    t_counter[left_char] += 1
                 
-
-        return '' if minLength == math.inf else s[substr_start: substr_start + minLength]
+                char = s[l]
+                l += 1
+                if char in t_frequency_map:
+                    if t_frequency_map[char] == 0:
+                        matched -= 1
+                    t_frequency_map[char] += 1
+                
+        
+        return "" if substr_max == math.inf else s[substr_start: substr_start + substr_max]
