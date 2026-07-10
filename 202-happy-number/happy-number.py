8ef7ebe17a700e433004aad5a6c8c1c7cc7447bc
Shahene Chaouachi <120350676+shahene@users.noreply.github.com>
2026-07-09 17:14:18 -0700

    Time: 3 ms (35.34%) | Memory: 19.2 MB (89.81%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -1,21 +1,13 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        if n == 1: return True 
-        fast, slow = n, n
-        fast, slow = self.convert_number(self.convert_number(fast)), self.convert_number(slow)
-        if slow == 1 or fast == 1: return True
-        while fast != slow:
-            if slow == 1 or fast == 1:
-                return True
-            fast = self.convert_number(self.convert_number(fast))
-            slow = self.convert_number(slow) 
-        return False
-    def convert_number(self, n: int) -> int:
-        digit_sum = 0
-        while n:
-            digit_sum += ((n % 10) ** 2)
-            n = n // 10
-        return digit_sum
-    
-
-
+        seen = set()
+        while n != 1:
+            if n in seen:
+                return False # cycle
+            seen.add(n)
+            # calc sum of squares of digits
+            total = 0
+            for num in str(n):
+                total += int(num) ** 2
+            n = total
+        return True
