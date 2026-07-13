e2bddf8e9e35302ce99aac8717be63fbb1ccc6e2
Shahene Chaouachi <120350676+shahene@users.noreply.github.com>
2026-07-12 18:49:07 -0700

    Time: 11 ms (83.15%) | Memory: 22.7 MB (34.98%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,14 +1,13 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        ans = []
-        sorted_map = collections.defaultdict(list)
+        '''
+        aet: [eat, tea]
+        '''
+        anagram_map = collections.defaultdict(list)
         for n in strs:
-            n_sorted = ''.join(sorted(n))
-            sorted_map[n_sorted].append(n)
-        
-        for j in sorted_map:
-            el = []
-            for n in sorted_map[j]:
-                el.append(n)
-            ans.append(el)
-        return ans
+            n_sorted = tuple(sorted(n))
+            anagram_map[n_sorted].append(n)
+        res = []
+        for n in anagram_map:
+            res.append(anagram_map[n])
+        return res 
