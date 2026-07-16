e6f075dcc79e11cc2b9c165cfc0c30fa7539e4b9
Shahene Chaouachi <120350676+shahene@users.noreply.github.com>
2026-07-16 12:09:16 -0700

    Time: 7 ms (91.47%) | Memory: 19.5 MB (29.35%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,10 +1,4 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t): return False
-        fixed_arr = [0] * 26
-        for s_char, t_char in zip(s, t):
-            fixed_arr[ord(s_char) - ord('a')] += 1
-            fixed_arr[ord(t_char) - ord('a')] -= 1
-        for check in fixed_arr:
-            if check != 0: return False
-        return True
+        s_map, t_map = collections.Counter(s), collections.Counter(t)
+        return s_map == t_map
