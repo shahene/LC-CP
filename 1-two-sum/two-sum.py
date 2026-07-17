6fad423e424e1e62da5673e5b7594ac934211624
Shahene Chaouachi <120350676+shahene@users.noreply.github.com>
2026-07-17 12:54:41 -0700

    Time: 1 ms (61.43%) | Memory: 20.6 MB (7.05%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,16 +1,22 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         '''
-        target = num1 + num2
-        num2 = target - num1
-        store target - current_num as keys in hashmap
-        w index of (current_num) num1 as value
+        given an array of integer nums
+        input: nums array and target
+        output: indices of two numbers such that they add up to target
+
+        match => since we are returning indices, it would not be ideal to sort the array
+              => can use a hash map
+              => nums[i] + nums[j] = target
+              => nums[j] = target - nums[i]
+              => store key as (target - nums[i])
+              => if we find match, make sure indices don't match in case of duplicates that add up to target
+              => assume solution exists
+              => can do it one pass 
         '''
         complement_map = {}
-        for i, n in enumerate(nums):
-            complement_map[target - n] = i
-        for i, n in enumerate(nums):
-            if n in complement_map and i != complement_map[n]:
-                return [i, complement_map[n]]
-                
-        
+        for i, num in enumerate(nums):
+            complement = target - num
+            if num in complement_map and i != complement_map[num]:
+                return [i, complement_map[num]]
+            complement_map[complement] = i
