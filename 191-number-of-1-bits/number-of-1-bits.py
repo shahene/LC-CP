c81151122a997e40aed50bed78f329f80e04c76f
Shahene Chaouachi <120350676+shahene@users.noreply.github.com>
2026-07-21 11:47:35 -0700

    Time: 0 ms (100.00%) | Memory: 19 MB (97.74%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.py
+++ 191-number-of-1-bits/number-of-1-bits.py
@@ -1,7 +1,19 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        num_bits = 0
+        '''
+        given a positive integer n, write a function that returns the number of set bits in its binary 
+        representation
+
+        (also known as the Hamming weight)
+        while x:
+            count_bits += (x & 1)
+            right shift to move to next bit >>
+        return count_bits
+        O(N) where N is the number of bits in binary rep. of n
+        O(1) space
+        '''
+        count_bits = 0
         while n:
-            num_bits += (n & 1)
+            count_bits += (n & 1)
             n >>= 1
-        return num_bits
+        return count_bits
