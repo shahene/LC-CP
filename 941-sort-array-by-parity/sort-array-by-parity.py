cf99564951bb3a6c7c275a014e1a11be48aea7b6
Shahene Chaouachi <120350676+shahene@users.noreply.github.com>
2026-07-22 12:40:48 -0700

    Time: 0 ms (100.00%) | Memory: 19.7 MB (53.19%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.py
+++ 941-sort-array-by-parity/sort-array-by-parity.py
@@ -1,14 +1,14 @@
 class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
-        res = [0] * len(nums)
         beg, end = 0, len(nums) - 1
         # how to find parity of number?
         # use
-        for n in nums:
-            if n & 1 == 0:
-                res[beg] = n
-                beg += 1
-            else:
-                res[end] = n
+        while beg < end:
+            if nums[beg] & 1 == 1:
+                tmp = nums[end]
+                nums[end] = nums[beg]
+                nums[beg] = tmp
                 end -= 1
-        return res
+            else:
+                beg += 1
+        return nums
