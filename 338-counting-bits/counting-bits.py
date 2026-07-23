4adfd66d5175fff21fb75a28eadcd8ed7aee47f2
Shahene Chaouachi <120350676+shahene@users.noreply.github.com>
2026-07-23 13:27:42 -0700

    Time: 31 ms (24.31%) | Memory: 20.1 MB (94.16%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -1,14 +1,13 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        res = []
+        res = [0] * (n + 1)
         for i in range(n + 1):
-            tot = 0
-            bitpatt = i
-            while bitpatt:
-                bit = bitpatt & 1
-                if bit == 1:
-                    tot += 1
-                bitpatt = bitpatt >> 1
-
-            res.append(tot)
+            c = self.count_single_number(i)
+            res[i] = c
         return res
+    def count_single_number(self, number: int):
+        count = 0
+        while number:
+            count += 1
+            number &= number - 1
+        return count
