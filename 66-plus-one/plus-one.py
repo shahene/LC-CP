7b0e2937cdb846d6532a671f38782f85588899c2
Shahene Chaouachi <120350676+shahene@users.noreply.github.com>
2026-07-27 13:26:07 -0700

    Time: 0 ms (100.00%) | Memory: 19.5 MB (18.23%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -1,7 +1,53 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        int_digits = int(''.join(str(num) for num in digits))
-        int_digits += 1
-        string_digits = str(int_digits)
-        string_digit_arr = [int(s) for s in string_digits]
-        return string_digit_arr
+        '''
+        input: array of digits (ordered from MSB to LSB (in left to right order))
+        output: array of digits after adding one arithmetically to the original input
+        
+        Input: digits = [19]
+        Output: [2, 0]
+
+        better to reverse array and do arithmetic with carry
+
+        code:
+        digits.reverse()
+        i, carry = 0, 0
+        while carry or i < len(nums):
+            res = (digits[i] + 1 + carry) % 10
+            carry = (res // 10)
+            result_arr.append(res)
+        result_arr.reverse()
+        return result_arr
+
+        input: digits = [1, 9]
+        digits = [9, 1]
+
+        result_arr = [0, 2]
+        carry = 0
+        result_arr = [2, 0]
+
+
+        '''
+        result = []
+        carry = 0
+        i = len(digits) - 1
+        while carry or i >= 0:
+            if i == len(digits) - 1:
+                total = digits[i] + 1 + carry
+                res = total % 10
+                result.append(res)
+                carry = total // 10
+            elif i != len(digits) - 1 and carry:
+                if i in range(len(digits)):
+                    total = digits[i] + carry
+                else:
+                    total = carry
+                res = total % 10
+                result.append(res)
+                carry = total // 10
+            else:
+                result.append(digits[i])
+            i -= 1
+        result.reverse()
+        return result
+        
