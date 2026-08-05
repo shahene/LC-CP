7cfcde0808a4e7b142e92aa1629211697e58d96b
Shahene Chaouachi <120350676+shahene@users.noreply.github.com>
2026-08-04 19:05:00 -0700

    Time: 82 ms (82.11%) | Memory: 31.1 MB (88.01%) - LeetSync

--- 155-min-stack/min-stack.py
+++ 155-min-stack/min-stack.py
@@ -2,24 +2,30 @@ class MinStack:
 
     def __init__(self):
         self.stack = []
+        self.min_contenders = []
         
-
-    def push(self, val: int) -> None:
-        self.stack.append(val)
+    def push(self, value: int) -> None:
+        self.stack.append(value)
+        if not self.min_contenders: 
+            self.min_contenders.append(value)
+            return
+        if value <= self.min_contenders[-1]: self.min_contenders.append(value)
 
     def pop(self) -> None:
-        self.stack.pop()
+        element = self.stack.pop()
+        if element == self.min_contenders[-1]: self.min_contenders.pop()
 
     def top(self) -> int:
         return self.stack[-1]
 
     def getMin(self) -> int:
-        return min(self.stack)
+        return self.min_contenders[-1]
+        
 
 
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
-# obj.push(val)
+# obj.push(value)
 # obj.pop()
 # param_3 = obj.top()
 # param_4 = obj.getMin()
