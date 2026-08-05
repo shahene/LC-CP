bc7c5b6a01f73f8496249a233faf8914abdbae89
Shahene Chaouachi <120350676+shahene@users.noreply.github.com>
2026-08-05 14:17:47 -0700

    Time: 56 ms (30.80%) | Memory: 20.8 MB (60.93%) - LeetSync

--- 774-maximum-depth-of-n-ary-tree/maximum-depth-of-n-ary-tree.py
+++ 774-maximum-depth-of-n-ary-tree/maximum-depth-of-n-ary-tree.py
@@ -8,15 +8,12 @@ class Node:
 import collections
 class Solution:
     def maxDepth(self, root: 'Node') -> int:
-        height = 0
-        if not root: return 0
-        queue = collections.deque([root])
-        while queue:
-            for _ in range(len(queue)):
-                n = queue.popleft()
-                for child in n.children:
-                    queue.append(child)
-            height += 1
-        return height
+        max_height = 0
+        if not root:
+            return 0
+        for n in root.children:
+            child_height = self.maxDepth(n)
+            max_height = max(max_height, child_height)
+        return 1 + max_height
             
         
