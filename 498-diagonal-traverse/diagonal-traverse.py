7d1e95479e1d1f1c4fc2330871335370b4d450a1
Shahene Chaouachi <120350676+shahene@users.noreply.github.com>
2026-08-09 13:33:38 -0700

    Time: 13 ms (44.77%) | Memory: 22.8 MB (9.31%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.py
+++ 498-diagonal-traverse/diagonal-traverse.py
@@ -1,55 +1,27 @@
+import collections
 class Solution:
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
-        '''
-        different row and col starting for each direction
-        
-        direction = up right
-        up and right as much as possible
-        1
-        then move col to the right
-        direction = down left 
-        down and left as much as possible
-        2 4
-        then move row "down"
-        direction = up right
-        up and to the right as much as possible
-        7 5 3 
-        then col cant be moved anymore so move row down
-        down and left as much as much as possible
-        6 8
-        row can't be moved down anymore so move col up
-        '''
+        # group diagonals by the sum of their indices
+        # iterate through the matrix normally
+        # dictionary that has keys as values -> keys will be r + c,values will be lists
+        # 0, 1, 2, 3, 4
+        # append to res while changing direction
+        # return res
+
         res = []
-        direction = True
         rows, cols = len(mat), len(mat[0])
-        last_valid_row, last_valid_col = 0, 0
-        current_row, current_col = 0, 0
-        while current_row in range(rows) and current_col in range(cols):
-            
-            # up right
+        diagonal_dictionary = collections.defaultdict(list)
+        for r in range(rows):
+            for c in range(cols):
+                index = r + c
+                diagonal_dictionary[index].append(mat[r][c])
+        
+        direction = False
+        for index in diagonal_dictionary:
             if direction:
-                iterative_row, iterative_col = current_row, current_col
-                while iterative_row in range(rows) and iterative_col in range(cols):
-                    res.append(mat[iterative_row][iterative_col])
-                    last_valid_row, last_valid_col = iterative_row, iterative_col
-                    iterative_row -= 1
-                    iterative_col += 1
-                if last_valid_col < cols - 1:
-                    current_col, current_row = last_valid_col + 1, last_valid_row
-                else:
-                    current_row, current_col = last_valid_row + 1, last_valid_col
+                res.extend(diagonal_dictionary[index])
                 direction = not direction
-            # down left
             else:
-                iterative_row, iterative_col = current_row, current_col
-                while iterative_row in range(rows) and iterative_col in range(cols):
-                    res.append(mat[iterative_row][iterative_col])
-                    last_valid_row, last_valid_col = iterative_row, iterative_col
-                    iterative_row += 1
-                    iterative_col -= 1
-                if last_valid_row < rows - 1:
-                    current_row, current_col = last_valid_row + 1, last_valid_col
-                else:
-                    current_col, current_row = last_valid_col + 1, last_valid_row
-                direction = not direction                    
-        return res 
+                res.extend(diagonal_dictionary[index][::-1])
+                direction = not direction
+        return res
