c6ed24b50591b1862f3b8a1ebf5d1f5661ee92ff
Shahene Chaouachi <120350676+shahene@users.noreply.github.com>
2026-08-11 13:47:43 -0700

    Time: 139 ms (28.45%) | Memory: 48.9 MB (79.19%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.py
+++ 435-non-overlapping-intervals/non-overlapping-intervals.py
@@ -1,15 +1,33 @@
-import math
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
-        non_overlapping = 0
-        intervals.sort(key=lambda x:x[0])
-        print(intervals)
-        prev_beg, prev_end = intervals[0]
+        '''
+        intervals[i] = [start_i, end_i]
+        return minimum number of intervals you need to remove to make the rest of the intervals non-overlapping
+        
+        [[1, 2], [1, 3], [2, 3], [3, 4]]
+
+        returns 1
+
+        b.start < a.end:
+            += 1
+        else:
+            a.end = b.end
+
+        [[1, 2], [1, 2], [1, 2]]
+        returns 2
+
+        [[1, 11], [1, 100], [2, 12], [11, 22]]
+        '''
+        count = 0
+        if not intervals: return count
+        intervals.sort(key=lambda x: (x[0], x[1]))
+        a_end = intervals[0][1]
         for i in range(1, len(intervals)):
-            n_beg, n_end = intervals[i]
-            if n_beg < prev_end:
-                prev_end = min(n_end, prev_end)
-                non_overlapping += 1
+            b_start, b_end = intervals[i]
+            if b_start < a_end:
+                a_end = min(a_end, b_end)
+                count += 1
             else:
-                prev_beg, prev_end = n_beg, n_end
-        return non_overlapping
+                a_end = b_end
+        return count
+        
