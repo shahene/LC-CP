9462d6acb24c8a2a7f03502b00f9a25d97cbfb6b
Shahene Chaouachi <120350676+shahene@users.noreply.github.com>
2026-08-11 13:21:17 -0700

    Time: 0 ms (100.00%) | Memory: 23.1 MB (28.08%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,31 +1,15 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        '''
-        intervals[i] = [start_i, end_i]
-        merge all overlapping intervals
-        return an array of non-overlapping intervals
-
-        sort intervals by start time
-        initial_interval_start
-        next end time <= prev_start time:
-            if true: need to merge -> merged_interval =[prev_start, max(next end, prev_end)]
-            if not: we merge current interval we have to the array
-        at the end, we have 1 more left over, just append to merged interval array
-        O(nlogn), auxiliary space -> O(1) 
-        '''
-        merged_intervals = []
-        intervals.sort()
-        prev_start, prev_end = intervals[0]
+        res = []
+        if not intervals: return res
+        intervals.sort(key=lambda x:x[0])
+        a_start, a_end = intervals[0][0], intervals[0][1]
         for i in range(1, len(intervals)):
-            cur_start, cur_end = intervals[i]
-            if cur_start <= prev_end:
-                prev_end = max(prev_end, cur_end)
-                print(prev_start, prev_end)
+            b_start, b_end = intervals[i][0], intervals[i][1]
+            if b_start <= a_end:
+                a_end = max(b_end, a_end)
             else:
-                merged_intervals.append([prev_start, prev_end])
-                prev_start, prev_end = intervals[i]
-            
-        merged_intervals.append([prev_start, prev_end])
-        return merged_intervals
-
-        return mered_intervals
+                res.append([a_start, a_end])
+                a_start, a_end = intervals[i]
+        res.append([a_start, a_end])
+        return res
