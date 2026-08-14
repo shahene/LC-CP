6256dbfa32221760b18cbcfc688256a789f7b79e
Shahene Chaouachi <120350676+shahene@users.noreply.github.com>
2026-08-14 15:07:47 -0700

    Time: 0 ms (100.00%) | Memory: 20.5 MB (27.30%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -5,32 +5,11 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        '''
-        input: head of a linked list
-        output: reversed list
-
-        edge cases: given 1 node as linkedlist
-        provided Node Class:
-        class Listnode:
-            def __init__(self, value, next=None):
-                self.value = value
-                self.next = next
-        
-        use a prev pointer to reasssign next pointers from the beginning of the list
         prev = None
-        tmp = current_node.next
-        current_node.next = prev
-        prev = current_node
-        current_node = tmp
-
-        return prev at the end
-        
-        '''
-        current = head
-        prev = None
-        while current:
-            tmp = current.next
-            current.next = prev
-            prev = current
-            current = tmp
+        curr = head
+        while curr:
+            tmp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = tmp
         return prev
