db86fbccaabdc5450dd29754a9bd73133a0854ac
Shahene Chaouachi <120350676+shahene@users.noreply.github.com>
2026-08-14 15:39:36 -0700

    Time: 7 ms (24.12%) | Memory: 19.4 MB (40.87%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -5,52 +5,18 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        '''
-        input: two non empty linked lists (heads)
-        output: linked list that represents the sum
-
-        edge case: 9, 9, 9, 9  9, 9, 9, 9
-
-        plan
-        5 + 2 => 7 
-        6 + 4 => 0
-        carry=
-
-        Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
-        Output: [8,9,,9,9 0,0,0,1]
-
-        carry = 0
-        while carry or l1 or l2:
-            value = l1.val + l2.val
-            if value > 9:
-                take least significant digit
-                using modulo operator 
-                // 
-            else:
-                add to new linked list
-        return new linked list
-        '''
         carry = 0
         dummy = ListNode(-1)
         curr = dummy
-        while carry or l1 or l2:
+        while l1 or l2 or carry:
             l1_val = l1.val if l1 else 0
             l2_val = l2.val if l2 else 0
-
-            current_sum = l1_val + l2_val + carry
-            
-            if current_sum > 9:
-                lsd = current_sum % 10
-                curr.next = ListNode(lsd)
-                msd = current_sum // 10
-                carry = msd
-            else:
-                curr.next = ListNode(current_sum)
-                carry = 0
+            if l1: l1 = l1.next
+            if l2: l2 = l2.next
+            total = l1_val + l2_val + carry
+            node_sum = total % 10
+            curr.next = ListNode(node_sum)
             curr = curr.next
-            if l1:
-                l1 = l1.next 
-            if l2:
-                l2 = l2.next
-
+            carry = total // 10
         return dummy.next
+
