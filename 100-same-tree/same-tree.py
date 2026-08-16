236ac9dd9e46b9adfc499a1cfd70be96a00012fb
Shahene Chaouachi <120350676+shahene@users.noreply.github.com>
2026-08-16 11:33:17 -0700

    Time: 0 ms (100.00%) | Memory: 19.3 MB (31.07%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -6,10 +6,23 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if not p and not q:
-            return True
-        if not p or not q or p.val != q.val:
-            return False
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-        
-        
+        '''
+        UMPIRE
+        understand
+        input: two binary tree roots: p and q
+        output: true or false depending on if the two trees are structurally identical and nodes have the same value
+
+        match:
+        dfs 
+
+        plan:
+        base case:
+        if not p and not q: return True
+        if not p or not q or p.val != q.val: return False
+        recurisve step:
+        check left and right subtrees of both roots
+        return isSameTree(p.left, q.left) and isSameTree(p.right, q.right)
+        '''
+        if not p and not q: return True
+        if not p or not q or p.val != q.val: return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(q.right, p.right)
