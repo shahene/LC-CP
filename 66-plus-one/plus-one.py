e4f0aa1366ce847023a81e9b2d86d9f79b027914
Shahene Chaouachi <120350676+shahene@users.noreply.github.com>
2026-08-16 20:51:54 -0700

    Time: 0 ms (100.00%) | Memory: 19.3 MB (18.61%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -1,53 +1,25 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        '''
-        input: array of digits (ordered from MSB to LSB (in left to right order))
-        output: array of digits after adding one arithmetically to the original input
-        
-        Input: digits = [19]
-        Output: [2, 0]
-
-        better to reverse array and do arithmetic with carry
-
-        code:
-        digits.reverse()
-        i, carry = 0, 0
-        while carry or i < len(nums):
-            res = (digits[i] + 1 + carry) % 10
-            carry = (res // 10)
-            result_arr.append(res)
-        result_arr.reverse()
-        return result_arr
-
-        input: digits = [1, 9]
-        digits = [9, 1]
-
-        result_arr = [0, 2]
-        carry = 0
-        result_arr = [2, 0]
+        if digits[-1] < 9:
+            digits[-1] += 1
+            return digits
 
 
-        '''
-        result = []
-        carry = 0
-        i = len(digits) - 1
-        while carry or i >= 0:
-            if i == len(digits) - 1:
-                total = digits[i] + 1 + carry
-                res = total % 10
-                result.append(res)
-                carry = total // 10
-            elif i != len(digits) - 1 and carry:
-                if i in range(len(digits)):
-                    total = digits[i] + carry
-                else:
-                    total = carry
-                res = total % 10
-                result.append(res)
-                carry = total // 10
+        carry = 1
+        for i in range(len(digits) - 1, -1, -1):
+            tot = digits[i] + carry
+            if tot > 9:
+                digits[i] = 0
+                carry = 1
             else:
-                result.append(digits[i])
-            i -= 1
-        result.reverse()
-        return result
-        
+                digits[i] = tot
+                return digits
+
+        res = [0] * (len(digits) + 1)
+        print(res)
+        print(digits)
+        res[0] = 1
+        print(res)
+        for i in range(1, len(digits)):
+            res[i] = digits[i - 1]
+        return res
