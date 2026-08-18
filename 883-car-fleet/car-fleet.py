64a1d5ed186c5c601f5e046b1a060bcbe4e9c51f
Shahene Chaouachi <120350676+shahene@users.noreply.github.com>
2026-08-18 13:46:31 -0700

    Time: 150 ms (77.89%) | Memory: 40.9 MB (80.88%) - LeetSync

--- 883-car-fleet/car-fleet.py
+++ 883-car-fleet/car-fleet.py
@@ -1,31 +1,23 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        
         '''
-        combine two arrays (position and speed)
-        sort new array based on position
-
-        number of fleets = 0
-        start from right
-        calculate time taken to reach target (target - current position) / speed
-        if next time is <= current time: 
-            continue
-        otherwise 
+        time taken to reach target = target - position / speed
+        [(0, 1), (3, 3), (5, 1), (8, 4), (10, 2)] => (1s, 1s), (7s, 3s, 2), (12s)
+        
+        calc time to reach target
+        if next time > curernt time:
             current time = next time
-            fleet += 1
-
-        [(position1, speed1), (position2, speed2)]
-
+            number_fleets += 1
         '''
-        fleets = 0
-        position_speed = [(p, s) for p, s in zip(position, speed)]
-        position_speed.sort(key=lambda x: x[0])
+        number_fleets = 0
+        position_speed = sorted([(p, s) for p, s in zip(position, speed)])
         current_time = -math.inf
-        for i in range(len(position_speed) - 1, -1 , -1):
+        for i in range(len(position_speed) - 1, -1, -1):
             p, s = position_speed[i]
             next_time = (target - p) / s
             if next_time > current_time:
                 current_time = next_time
-                fleets += 1
-
-        return fleets
-        
+                number_fleets += 1
+            
+        return number_fleets    
