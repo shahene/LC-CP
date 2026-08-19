d59d8ea777b84b5bd5e95b3042d59950af43ec3a
Shahene Chaouachi <120350676+shahene@users.noreply.github.com>
2026-08-18 19:54:11 -0700

    Time: 0 ms (100.00%) | Memory: 19.3 MB (27.27%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.py
+++ 678-valid-parenthesis-string/valid-parenthesis-string.py
@@ -3,25 +3,21 @@ class Solution:
         '''
         
         '''
-        stack_char = []
-        stack_star = []
+        stack_char, stack_star = [], []
+
         for i, n in enumerate(s):
             if n == '(':
-                stack_char.append((')', i))
+                stack_char.append(i)
             elif n == '*':
-                stack_star.append((n, i))
+                stack_star.append(i)
             else:
-                if stack_char:
-                    stack_char.pop()
-                elif stack_star:
-                    char, index = stack_star.pop()
-                    if index > i:
-                        return False
-                else:
-                    return False
+                if stack_char: stack_char.pop()
+                elif stack_star: stack_star.pop()
+                else: return False
+
         while stack_star and stack_char:
-            char, index = stack_star.pop()
-            char_1, index_1 = stack_char.pop()
-            if index < index_1: return False
+            index_star, index_char = stack_star.pop(), stack_char.pop()
+            if index_star < index_char: return False
+
         return len(stack_char) == 0
             
