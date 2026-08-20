1a478ba44c7bc441e8bb583c0bd882fca88a1092
Shahene Chaouachi <120350676+shahene@users.noreply.github.com>
2026-08-20 12:45:31 -0700

    Time: 7 ms (16.87%) | Memory: 20.2 MB (99.10%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.py
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -6,12 +6,17 @@
 #         self.right = right
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        def find_height(node):
-            if not node: return 0
-            return 1 + max(find_height(node.left), find_height(node.right))
-        
+        '''
+        for every node, check its left and right subtrees and find its height
+        if difference between left and right subtree heights is greater than 1: return false
+        if we exhaust the search and we never hit that case: return true
+        '''
         if not root: return True
-        left_height = find_height(root.left)
-        right_height = find_height(root.right)
-        if abs(left_height - right_height) > 1: return False
+        leftHeight = self.find_height(root.left)
+        rightHeight = self.find_height(root.right)
+        if abs(leftHeight - rightHeight) > 1: return False
         return self.isBalanced(root.left) and self.isBalanced(root.right)
+    def find_height(self, node):
+        if not node: return 0
+        return 1 + max(self.find_height(node.left), self.find_height(node.right))
+    
