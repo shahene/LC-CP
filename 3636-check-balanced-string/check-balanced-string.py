f38c3a03eb8798be9b4299ffda231fc9b5b78b8d
Shahene Chaouachi <120350676+shahene@users.noreply.github.com>
2026-08-20 13:29:21 -0700

    Time: 0 ms (100.00%) | Memory: 19.2 MB (51.73%) - LeetSync

--- 3636-check-balanced-string/check-balanced-string.py
+++ 3636-check-balanced-string/check-balanced-string.py
@@ -1,9 +1,9 @@
 class Solution:
     def isBalanced(self, num: str) -> bool:
-        even_sum, odd_sum = 0, 0
+        total_sum = 0
         for i, n in enumerate(num):
             if i % 2 == 0: 
-                even_sum += (ord(n) - ord('0'))
+                total_sum += (ord(n) - ord('0'))
             else:
-                odd_sum += (ord(n) - ord('0'))
-        return even_sum == odd_sum
+                total_sum -= (ord(n) - ord('0'))
+        return total_sum == 0
