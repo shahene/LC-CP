6ca2477b763ec37d8a0912d90a00299c56d672d2
Shahene Chaouachi <120350676+shahene@users.noreply.github.com>
2026-08-21 18:57:06 -0700

    Time: 171 ms (48.30%) | Memory: 20.8 MB (12.15%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,17 +1,28 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        l, r = 1, max(piles)
-        res = r
-        while l <= r:
-            k = (l + r) // 2
-            hours = 0
-            for pile in piles:
-                hours += (math.ceil(pile / k))
-
-            if hours <= h:
-                res = min(res, k)
-                r = k - 1
+        '''
+        [30,11,23,4,20], h = 5
+        left = 1, right = 30, mid_k = 15
+        '''
+        left, right = 1, max(piles)
+        min_k = math.inf
+        while left <= right:
+            mid_k = (left + right) // 2
+            print(mid_k)
+            valid = self.koko_valid(piles, mid_k, h)
+            print(valid)
+            if valid:
+                right = mid_k - 1
+                min_k = min(min_k, mid_k)
             else:
-                l = k + 1
+                left = mid_k + 1
+        return min_k
+            
 
-        return res
+
+
+    def koko_valid(self, piles, k, h):
+        hours_taken = 0
+        for n in piles:
+            hours_taken += (math.ceil(n / k))
+        return hours_taken <= h
