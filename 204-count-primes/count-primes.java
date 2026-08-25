a1ce30f7efedbde4a53c0a5938889d8e23b8c9c8
Shahene Chaouachi <120350676+shahene@users.noreply.github.com>
2026-08-25 13:23:46 -0700

    Time: 781 ms (5.04%) | Memory: 80.1 MB (6.26%) - LeetSync

--- 204-count-primes/count-primes.java
+++ 204-count-primes/count-primes.java
@@ -1,24 +1,34 @@
 class Solution {
     public int countPrimes(int n) {
-        if (n <= 2) return 0;
-        boolean[] isPrime = new boolean[n];
+        /*
+        Sieve of Eratosthenes
+        every composite number has at least one prime factor
+        for each prime factor starting from 2, ignore multiples of that prime factor as they will be composite
+        use a boolean array
+        where True represents a certain composite number
+        and False represents a potential prime number
+        [False, False, False, ...]
+        starting at index 2, change the value at multiple index i in boolean array to True
+        return number of False values in array
 
-        // false = potentially prime
-        // true = composite
-
+        n = 10
+        [False, False, False, False, False, False, False, False, False, False]
+        */
+        boolean[] booleanArray = new boolean[n];
         int primeCount = 0;
         for (int i = 2; i < n; i++) {
-            if (isPrime[i]) {
-                continue;
-            }
-            primeCount++;
-            if ((long) i * i < n) {
-                for (int j = i * i; j < n; j += i) {
-                    isPrime[j] = true;
+            if (booleanArray[i]) continue;
+            primeCount += 1;
+            int primeStart = i + i;
+            if (primeStart < n) {
+                for (int k = primeStart; k < n; k += i) {
+                    booleanArray[k] = true;
                 }
             }
+           
+
         }
         return primeCount;
     }
-    
+     
 }
