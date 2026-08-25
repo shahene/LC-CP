01337b277595533497136fc1179bce47cd28c3d1
Shahene Chaouachi <120350676+shahene@users.noreply.github.com>
2026-08-25 11:44:58 -0700

    Time: 0 ms (100.00%) | Memory: 19.5 MB (35.09%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,9 +1,9 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         if len(nums) == 0: return [[]]
-        perms = self.permute(nums[1:])
+        permutations = self.permute(nums[1:])
         res = []
-        for p in perms:
+        for p in permutations:
             for i in range(len(p) + 1):
                 p_copy = p.copy()
                 p_copy.insert(i, nums[0])
