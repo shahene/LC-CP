6514cca97551319506251e11042a1022c175b860
Shahene Chaouachi <120350676+shahene@users.noreply.github.com>
2026-08-25 12:38:02 -0700

    Time: 46 ms (5.86%) | Memory: 19.5 MB (19.29%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,20 +1,13 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         if x < 0: return False
-         # if you mod by 10, we get 1s place (right value)
-         # 121 / 100 => 1 (rounding down)
-         # chopping off: 
-         # 121/10 => 12
-         # 121%100 = 21, then 21 / 10 => 2
-        divide_val = 1
-        while x >= 10 * divide_val:
-            divide_val  *= 10
-        while x:
-            right = x % 10
-            left = x // divide_val
-            
-            if left != right: return False
-
-            x = (x % divide_val) // 10
-            divide_val = divide_val / 100
-        return True
+        if x == 0: return True
+        reversed_x = self.reverse_number(x)
+        return x == self.reverse_number(x)
+    def reverse_number(self, n):
+        digits = int(math.log10(n)) + 1
+        return self.helper(n, digits)
+    def helper(self, n, digits):
+        if n % 10 == n: return n
+        rem = n % 10
+        return rem * (10**(digits - 1)) + self.helper(n // 10, digits - 1)
