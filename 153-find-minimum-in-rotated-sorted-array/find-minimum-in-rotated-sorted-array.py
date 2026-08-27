e7fab9f164a2c408ddeb414a4d703d700aeab362
Shahene Chaouachi <120350676+shahene@users.noreply.github.com>
2026-08-27 15:06:49 -0700

    Time: 0 ms (100.00%) | Memory: 19.6 MB (5.06%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -1,13 +1,23 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        # go right if left side is unsorted
-        # go left otherwise
+        '''
+        [0, 1, 2, 4, 5, 6, 7]
+        rotated 4 times
+        [4, 5, 6, 7, 0, 1, 2]
+        return miimum element in O(log n) time
+
+        so binary search
+        
+        [nums[n-4], nums[n-3], nums[n-2], nums[n-1], nums[n-6], nums[n-5], nums[n-4]]
+        '''
+        minimum_val = math.inf
         l, r = 0, len(nums) - 1
-        minimum = nums[0]
-        while l < r:
-            mid = l + (r - l) // 2
-            if nums[mid] > nums[r]:
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[l] <= nums[mid]:
+                minimum_val = min(minimum_val, nums[l])
                 l = mid + 1
             else:
-                r = mid
-        return nums[l]
+                minimum_val = min(minimum_val, nums[mid])
+                r = mid - 1
+        return minimum_val
