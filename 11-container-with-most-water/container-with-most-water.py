ad03efdaffa0fd73cabcc5a23e994c0f498953e5
Shahene Chaouachi <120350676+shahene@users.noreply.github.com>
2026-09-01 00:08:49 -0700

    Time: 57 ms (60.67%) | Memory: 29.5 MB (67.68%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,26 +1,41 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         '''
-        given an integer array height of length n. There are n vertical lines 
-        drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i])
-        find two lines that together with the x-axis form a container, such that the 
-        container contains the most water
-        return maximum amount of water
+        given an integer height of length n
+        n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and
+        (i, height[i])
 
-        greedy:
-        two pointers, start from both ends
-        min_height from either endpoint is the constraint
-        container would be (r - l) * min_height
-        increment the pointer that points to the lower height [greedy]
-        
-        time: O(n), space O(1)
+        find two lines that together with the x-axis form a container such that the container
+        contains the most water
+        return maximum amount of water a container can store
+
+        [1, 2, 3, 4, 5]
+        l = 0
+        r = 4
+
+        input: list of height 
+        output: max area of container
+
+        algorithm:
+        we can start at both ends of the array
+        we can calculate area, keep running track of max area found
+        we want pointer that points to the smaller height to move (either left or right)
+        greedy like solution with two pointers
+
+        area = (r - l) * min(height[l], height[r])
+
+        O(N) time because we only process the array once
+        O(1) constant space
         '''
         l, r = 0, len(height) - 1
-        max_container = 0
+        max_area = -math.inf
         while l < r:
-            max_container = max(max_container, min(height[l], height[r]) * (r - l))
-            if height[l] < height[r]:
+            left_height, right_height = height[l], height[r]
+            cur_area = (r - l) * min(left_height, right_height)
+            max_area = max(max_area, cur_area)
+            if left_height < right_height:
                 l += 1
             else:
                 r -= 1
-        return max_container
+    
+        return max_area
