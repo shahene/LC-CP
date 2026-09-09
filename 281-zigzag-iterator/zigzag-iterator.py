b9b32339e46057b0db64f1a9fe3184d9eb261206
Shahene Chaouachi <120350676+shahene@users.noreply.github.com>
2026-09-08 20:55:40 -0700

    Time: 51 ms (74.76%) | Memory: 19.3 MB (100.00%) - LeetSync

--- 281-zigzag-iterator/zigzag-iterator.py
+++ 281-zigzag-iterator/zigzag-iterator.py
@@ -2,33 +2,26 @@ class ZigzagIterator:
     def __init__(self, v1: List[int], v2: List[int]):
         self.v1 = v1
         self.v2 = v2
-        self.merged_list = []
-        self.l_index, self.r_index = 0, 0
-        self.bool = True
-        while self.l_index < len(self.v1) and self.r_index < len(self.v2):
-            if self.bool:
-                self.merged_list.append(self.v1[self.l_index])
-                self.l_index += 1
-            else:
-                self.merged_list.append(self.v2[self.r_index])
-                self.r_index += 1
-            self.bool = not self.bool
-        while self.l_index < len(self.v1):
-            self.merged_list.append(self.v1[self.l_index])
-            self.l_index += 1
-        while self.r_index < len(self.v2):
-            self.merged_list.append(self.v2[self.r_index])
-            self.r_index += 1
-        self.global_index = 0
-
-    def next(self) -> int:
-        number = self.merged_list[self.global_index]
-        self.global_index += 1
-        return number
-            
+        self.v1_ptr = 0
+        self.v2_ptr = 0
+        self.v1_turn = True
 
     def hasNext(self) -> bool:
-        return self.global_index < len(self.merged_list)
+        return self.v1_ptr < len(self.v1) or self.v2_ptr < len(self.v2)
+        
+
+    def next(self) -> int:
+        if self.v1_ptr >= len(self.v1): self.v1_turn = False
+        if self.v2_ptr >= len(self.v2): self.v1_turn = True
+
+        if self.v1_turn:
+            returned_number = self.v1[self.v1_ptr]
+            self.v1_ptr += 1
+        else:
+            returned_number = self.v2[self.v2_ptr]
+            self.v2_ptr += 1
+        self.v1_turn = not self.v1_turn
+        return returned_number
 
 # Your ZigzagIterator object will be instantiated and called as such:
 # i, v = ZigzagIterator(v1, v2), []
