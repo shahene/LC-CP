74af9afd96db572da8f4803078d3a0b2d67d6b65
Shahene Chaouachi <120350676+shahene@users.noreply.github.com>
2026-09-15 18:09:46 -0700

    Time: 0 ms (100.00%) | Memory: 19.3 MB (65.41%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -4,32 +4,13 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        # 5 - 2 = 3 - 1 => index of node to remove (if we were to think of LL as an array)
-        # remove by reassinging pointers of prev
-        # 1, 1, 1, 1, 1, 1, 1, 1
-        # len(ll) - (n - 1)
-        # reassign pointers at that index
-        length = 0
-        curr = head
-        while curr:
-            length += 1
-            curr = curr.next
-        index = 0
-        curr = head
-        if length == 1 and n == 1: return None
-
-        while index != length - (n + 1) and curr:
-            index += 1
-            curr = curr.next
-        if curr and curr.next:
-            curr.next = curr.next.next
-        else:
-            index = length - (n+1)
-            curr = head
-            while index != 0:
-                curr = curr.next
-
-                index += 1
-            return curr
-        return head
+    def removeNthFromEnd(self, head: ListNode | None, n: int) -> ListNode | None:
+        dummy = ListNode(0, head)
+        fast = slow = dummy
+        for _ in range(n + 1):
+            fast = fast.next
+        while fast:
+            fast = fast.next
+            slow = slow.next
+        slow.next = slow.next.next
+        return dummy.next
