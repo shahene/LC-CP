f586cb7cadc000fa9752532c1266132f00190ae4
Shahene Chaouachi <120350676+shahene@users.noreply.github.com>
2026-09-22 13:00:39 -0700

    Time: 768 ms (30.23%) | Memory: 19.2 MB (92.44%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,29 +1,38 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         '''
-        pretend that each character is middle
-        check left and right if they are palindromes
-        goes down to O(n^2)
-        for even length palindrome edge case:
-            set l to i and r to i + 1
-            and expand with two chars instead of 1 (like in odd length)
+        treat each character as middle
+        expand outward 
+        keep track of starting_index and ending_index
+        check odd and even length strings
         '''
-        n = len(s)
-        longest = ''
-        def expand(l, r):
-            while l in range(n) and r in range(n) and s[l] == s[r]:
-                l -= 1
-                r += 1
-            return s[l + 1: r]
-
-        for i in range(n):
-            odd_palindrome = expand(i, i)
-            even_palindrome = expand(i, i + 1)
-            if len(odd_palindrome) > len(longest):
-                longest = odd_palindrome
-            if len(even_palindrome) > len(longest):
-                longest = even_palindrome
-        return longest
+        starting_longest, ending_longest = -1, -1
+        max_length = 0
+        for i in range(len(s)):
+            # odd
+            current_length = 1
+            left_char, right_char = i - 1, i + 1
+            while left_char in range(len(s)) and right_char in range(len(s)) and s[left_char] == s[right_char]:
+                left_char -= 1
+                right_char += 1
+                current_length += 2
+            if current_length > max_length:
+                starting_longest = left_char + 1
+                ending_longest = right_char
+                max_length = current_length
+            # even
+            current_length = 0
+            left_char, right_char = i, i + 1
+            while left_char in range(len(s)) and right_char in range(len(s)) and s[left_char] == s[right_char]:
+                left_char -= 1
+                right_char += 1
+                current_length += 2
+            if current_length > max_length:
+                starting_longest = left_char + 1
+                ending_longest = right_char
+                max_length = current_length
+        if starting_longest == -1: return ''
+        return s[starting_longest: ending_longest]
 
 
             
