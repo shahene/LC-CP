67b27a2a5854b53659c91765756b4a893c5a02da
Shahene Chaouachi <120350676+shahene@users.noreply.github.com>
2026-09-22 00:18:10 -0700

    Time: 32 ms (11.52%) | Memory: 20 MB (68.77%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -1,17 +1,23 @@
 class Solution:
-    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        # explore islands with dfs, keeping track of max size
-        max_islands = 0
-        total_r, total_c = len(grid), len(grid[0])
-        for r in range(total_r):
-            for c in range(total_c):
-                if grid[r][c] == 1:
-                    max_islands = max(max_islands, self.dfs_island(grid, r, c, total_r, total_c))
-        return max_islands
-        return max_islands
-    def dfs_island(self, grid, r, c, total_r, total_c):
-        if r >= total_r or c >= total_c or r < 0 or c < 0 or grid[r][c] == 0:
-            return 0
-        grid[r][c] = 0
-        return self.dfs_island(grid, r + 1, c, total_r, total_c) + self.dfs_island(grid, r - 1, c, total_r, total_c) \
-        + self.dfs_island(grid, r, c - 1, total_r, total_c) + self.dfs_island(grid, r, c + 1, total_r, total_c) + 1
+    def maxAreaOfIsland(self, grid: list[list[int]]) -> int:
+        visited = set()
+        maximum = 0
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                size = self.exploreSize(grid, r, c, visited)
+                maximum = max(maximum, size)
+        return maximum
+    
+    def exploreSize(self, grid, r, c, visited):
+        row_inbound = 0 <= r < len(grid)
+        col_inbound = 0 <= c < len(grid[0])
+        if not row_inbound or not col_inbound: return 0
+        if grid[r][c] == 0: return 0
+        if (r, c) in visited: return 0
+        visited.add((r, c))
+        size = 1
+        size += self.exploreSize(grid, r + 1, c, visited)
+        size += self.exploreSize(grid, r - 1, c, visited)
+        size += self.exploreSize(grid, r, c + 1, visited)
+        size += self.exploreSize(grid, r, c - 1, visited)
+        return size
